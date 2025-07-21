ace831b9ab0021ac59e47448a8cbd1c38aeb0a81
schammahmuhizi <38748323+schammahmuhizi@users.noreply.github.com>
2025-07-21 21:52:50 +0200

    Fixing categorie-membre controller:

--- app/Http/Controllers/CategorieMembreController.php
+++ app/Http/Controllers/CategorieMembreController.php
@@ -12,36 +12,36 @@ use Illuminate\Http\Response;
 
 class CategorieMembreController extends Controller
 {
-    public function index(Request $request): Response
+    public function index(Request $request)
     {
         $categorieMembres = CategorieMembre::all();
 
-        return new CategorieMembreCollection($categorieMembres);
+        return sendResponse($categorieMembres, 'Catégories de membres récupérées avec succès');
     }
 
-    public function store(CategorieMembreStoreRequest $request): Response
+    public function store(CategorieMembreStoreRequest $request)
     {
         $categorieMembre = CategorieMembre::create($request->validated());
 
-        return new CategorieMembreResource($categorieMembre);
-    }
+        return sendResponse($categorieMembre, 'Catégorie de membre créée avec succès');
+    }  
 
-    public function show(Request $request, CategorieMembre $categorieMembre): Response
+    public function show(Request $request, CategorieMembre $categorieMembre)
     {
-        return new CategorieMembreResource($categorieMembre);
+        return sendResponse($categorieMembre, 'Catégorie de membre récupérée avec succès');
     }
 
-    public function update(CategorieMembreUpdateRequest $request, CategorieMembre $categorieMembre): Response
+    public function update(CategorieMembreUpdateRequest $request, CategorieMembre $categorieMembre)
     {
         $categorieMembre->update($request->validated());
 
-        return new CategorieMembreResource($categorieMembre);
+        return sendResponse($categorieMembre, 'Catégorie de membre mise à jour avec succès');
     }
 
-    public function destroy(Request $request, CategorieMembre $categorieMembre): Response
+    public function destroy(Request $request, CategorieMembre $categorieMembre)
     {
         $categorieMembre->delete();
 
-        return response()->noContent();
+        return sendResponse(null, 'Catégorie de membre supprimée avec succès');
     }
 }
