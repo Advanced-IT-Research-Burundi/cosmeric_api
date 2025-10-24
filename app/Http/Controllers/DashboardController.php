4683cb8263f8a833ba1b7ad89d470614b4034fac
Jean-Lionel <nijeanlionel@gmailcom>
2025-10-24 12:37:35 +0200

    dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Assistance;
 use App\Models\Cotisation;
 use App\Models\Credit;
 use App\Models\Membre;
@@ -16,12 +17,26 @@ class DashboardController extends Controller
         return response()->json([
             'success' => true,
             'data' => [
-                'membres' => Membre::count(),
-                'cotisations' => Cotisation::count(),
-                'credits' => Credit::count(),
-                'remboursements' => Remboursement::count(),
-
-                'users' => User::count(),
+//                 Nshak mur dashboard haze kw afficha ibi :
+// 1. Cotisation de ce mois
+// 2. ⁠cotisation cumulées(total guhera au début)
+// 3. ⁠membres actifs/inactifs
+// 4. ⁠crédits accordés
+// 5. ⁠dettes en attente
+// 6. Crédit en cours
+// 6. ⁠demandes assistance
+// 7. ⁠taux recouvrement
+// 8. ⁠solde disponible
+                'cotisation_ce_mois' => Cotisation::whereMonth('created_at', now()->month)->count(),
+                'cotisation_cumulees' => Cotisation::count(),
+                'membres_actifs' => Membre::where('status', 'actif')->count(),
+                'membres_inactifs' => Membre::where('status', 'inactif')->count(),
+                'credits_accordes' => Credit::count(),
+                'dettes_en_attente' => Remboursement::where('status', 'en attente')->count(),
+                'credits_en_cours' => Credit::where('status', 'en cours')->count(),
+                'demandes_assistance' => Assistance::count(),
+                'taux_recouvrement' => 0,
+                'solde_disponible' => 0,
             ],
         ]);
     }
